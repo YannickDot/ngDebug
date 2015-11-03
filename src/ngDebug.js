e793d5e2f6c78208207fee46f866d64a03bb1e66
YannickDot <yannick.levif@gmail.com>
2015-11-03 10:42:14 +0100

    edit method names

--- src/ngDebug.js
+++ src/ngDebug.js
@@ -44,27 +44,27 @@
 
 	var isString = function(t) { return (Object.prototype.toString.call(t) === '[object String]'); };
 
-	ngDebug.scope = function scope(targetNode) {
+	ngDebug.getScope = function getScope(targetNode) {
 		var elem = isString(targetNode) ? document.querySelector(targetNode) : targetNode;
 		return angular.element(elem).scope();
 	};
 
-	ngDebug.rootScope = function rootScope() {
+	ngDebug.getRootScope = function getRootScope() {
 		var html = document.querySelector('html');
 		return angular.element(html).scope();
 	};
 
-	ngDebug.isolateScope = function isolateScope(targetNode) {
+	ngDebug.getIsolateScope = function getIsolateScope(targetNode) {
 		var elem = isString(targetNode) ? document.querySelector(targetNode) : targetNode;
 		return angular.element(elem).isolateScope();
 	};
 
-	ngDebug.controller = function controller(targetNode) {
+	ngDebug.getController = function getController(targetNode) {
 		var elem = isString(targetNode) ? document.querySelector(targetNode) : targetNode;
 		return angular.element(elem).controller();
 	};
 
-	ngDebug.service = function service(serviceName) {
+	ngDebug.getService = function getService(serviceName) {
 		var html = document.querySelector('html');
 		return angular.element(html).injector().get(serviceName);
 	};
@@ -80,24 +80,24 @@
 		// Credits: Adam's answer in http://stackoverflow.com/a/20786262/69362
 		// Paste this in browser's console
 		var $rootScope = angular.element(document.querySelectorAll("[ui-view]")[0]).injector().get('$rootScope');
-		 
+
 		$rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
 		console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
 		});
-		 
+
 		$rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
 		console.log('$stateChangeError - fired when an error occurs during transition.');
 		console.log(arguments);
 		});
-		 
+
 		$rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
 		console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
 		});
-		 
+
 		$rootScope.$on('$viewContentLoaded',function(event){
 		console.log('$viewContentLoaded - fired after dom rendered',event);
 		});
-		 
+
 		$rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
 		console.log('$stateNotFound '+unfoundState.to+' - fired when a state cannot be found by its name.');
 		console.log(unfoundState, fromState, fromParams);
